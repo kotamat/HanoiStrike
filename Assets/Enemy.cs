d7ddc3649bc99cf2c101deee3c472ac6f29e0345
松本 宏太 <matsumotokouta@matsumoto-no-MacBook-Air-4.local>
2013-12-22 18:12:48 +0700

    my first commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+	public Transform enemy;
+	public float life = 100;
+	// Use this for initialization
+	void Start () {
+
+	//	Instantiate(enemy,Vector2(Random.Range(-4,4),Random.Range(-7,7)), transform.rotation);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D obj){
+		if(obj.gameObject.name == "Player"){
+			life -= obj.gameObject.GetComponent<Player>().damage;
+		}
+
+		if(life <= 0){
+			Destroy(gameObject);
+		}
+
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float force = 100;
+	public float damage = 10;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+		float x = Input.GetAxis("Horizontal") * force;
+		float y = Input.GetAxis("Vertical") * force;
+		rigidbody2D.AddForce(new Vector2(x, y));
+
+	}
+
+}
